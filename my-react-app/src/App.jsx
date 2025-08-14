480d2b99be860ec2e1306936d9df968bd9900dbd
72 <72sevenzy2@gmail.com>
2025-08-14 06:01:18 -0800

    added components folder for readability

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import './stylesheets/App.css';
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import Notf from './notfound';
-import Home from './home';
-import UserProfs from './userprof';
+import Notf from './components/notfound';
+import Home from './components/home';
+import UserProfs from './components/userprof';
 import { Routs } from './routes';
 
 function App() {
